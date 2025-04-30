a70426a50a3dabe496cd36913fdfb51e60149294
srujan703 <srujankumar_lolam@epam.com>
2025-04-30 15:32:32 +0530

    Partition sum

---
+++ DP/Week-3/partitionSum.py
@@ -0,0 +1,3 @@
+# This is almost same as subsetSum , if the total sum of the array is odd , then we cannot divide or partition
+# it into two subsets whose total sum is equal . But if the total sum is even , target sum = total sum // 2
+# because s1 + s2 = s (but s1 = s2 ) ==> s1 = s/2
