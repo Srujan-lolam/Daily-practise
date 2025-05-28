091f93fbb92a761cb6a38c260f520b77ed4e1ffd
srujan703 <srujankumar_lolam@epam.com>
2025-05-28 12:51:08 +0530

    tabulation method done

--- DP/Week-4/unbound_knapsack.py
+++ DP/Week-4/unbound_knapsack.py
@@ -38,4 +38,27 @@
 # print(unbound(wt,val,W,n-1,dp))
 
 #APPROACH - 3 (TABULATION)
+def unbound(wt,val,W,dp,n):
+    try:
+        for weight in range(W+1):
+            if weight%wt[0] == 0:
+                dp[0][weight] = (weight//wt[0])*val[0]
+        for i in range(1,n):
+            for j in range(1,W+1):
+                not_pick = dp[i-1][j]
+                pick = 0
+                if wt[i] <= j:
+                    pick =val[i] + dp[i][j-wt[i]]
+                dp[i][j] = max(pick,not_pick)
+        print(dp)
+        return dp[n-1][W]
+    except Exception as e :
+        print("Exception caught = ",e)
 
+
+wt = [0, 4, 6]
+val = [5, 11, 13]
+W = 10
+n = len(wt)
+dp = [[0 for _ in range(W+1)] for _ in range(n)]
+print(unbound(wt,val,W,dp,n))
